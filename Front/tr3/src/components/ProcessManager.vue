0cdd0d4a196913a4d5240fd3ebc0ca3b9e4c432b
a23marrojgon <a23marrojgon@inspedralbes.cat>
2025-03-10 09:24:09 +0100

    Front

---
+++ Front/tr3/src/components/ProcessManager.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-card>
+      <v-card-title>Gestor de Procesos</v-card-title>
+      <v-card-text>
+        <v-list>
+          <v-list-item v-for="(process, index) in processes" :key="index">
+            <v-list-item-content>
+              <v-list-item-title>{{ process.name }}</v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn @click="toggleProcess(index)" :color="process.active ? 'error' : 'success'">
+                {{ process.active ? 'Detener' : 'Iniciar' }}
+              </v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+      </v-card-text>
+    </v-card>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ProcessManager',
+    data() {
+      return {
+        processes: [
+          { name: 'Proceso 1', active: false },
+          { name: 'Proceso 2', active: true },
+          { name: 'Proceso 3', active: false },
+        ]
+      }
+    },
+    methods: {
+      toggleProcess(index) {
+        this.processes[index].active = !this.processes[index].active
+      }
+    }
+  }
+  </script>
+  
---
+++ Front/tr3/src/pages/procesos.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h1>Gestión de Procesos</h1>
+      <ProcessManager />
+      <v-btn class="mt-4" color="primary" to="/assets">
+        Ir a Gestión de Assets
+      </v-btn>
+    </div>
+  </template>
+  
+  <script>
+  import ProcessManager from '@/components/ProcessManager.vue'
+  
+  export default {
+    name: 'ProcesosPage',
+    components: {
+      ProcessManager
+    }
+  }
+  </script>
+  
